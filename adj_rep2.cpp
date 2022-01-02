b0a5b74c4bea5c0b1a12b9e24137b61f6d0ae2d4
dsdjoy4@gmail.com <dsdjoy4@gmail.com>
2022-01-02 15:56:28 +1100

    adjacency list representation practice

---
+++ adj_rep2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+
+class Graph {
+public:
+    int V;
+    list<int> *l;
+
+    Graph(int v) {
+        this->V = v;
+        this->l = new list<int> [this->V];
+    }
+
+    void addEdge(int i, int j, bool undir = true) {
+        this->l[i].push_back(j);
+        if (undir) {
+            this->l[j].push_back(i);
+        }
+    }
+
+    void showGraph() {
+        for (int i = 0; i < this->V; i++) {
+            cout << i << ": ";
+            for (int vertex: this->l[i]) {
+                cout << vertex << ", ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main(void) {
+    Graph g(6);
+	g.addEdge(0,1);
+	g.addEdge(0,4);
+	g.addEdge(2,1);
+	g.addEdge(3,4);
+	g.addEdge(4,5);
+	g.addEdge(2,3);
+	g.addEdge(3,5);
+	g.showGraph();
+
+
+    return 0;
+}
