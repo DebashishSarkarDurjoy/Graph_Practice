dfb66ea73ba6347a5bf3c47464bf03cf1790f39d
dsdjoy4@gmail.com <dsdjoy4@gmail.com>
2022-01-03 15:00:06 +1100

    bfs shortest path code implemented

---
+++ bfs_shortest_path.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+#include<list>
+#include<queue>
+using namespace std;
+
+class Graph {
+public:
+    int V;
+    list<int> *l;
+
+    Graph(int v) {
+        this->V = v;
+        this->l = new list<int> [this->V];
+    }
+
+    void addEdge(int i, int j, bool undir = true) {
+        this->l[i].push_back(j);
+        if (undir) {
+            this->l[j].push_back(i);
+        }
+    }
+
+    void showGraph() {
+        for (int i = 0; i < this->V; i++) {
+            cout << i << ": ";
+            for (int vertex: this->l[i]) {
+                cout << vertex << ", ";
+            }
+            cout << endl;
+        }
+    }
+
+    void bfs(int source, int dest = -1) {
+        queue<int> q;
+        int *dist = new int[this->V]{0};
+        int *parent = new int[this->V]{-1};
+        bool *visited = new bool[this->V]{false};
+        q.push(source);
+        dist[source] = 0;
+        parent[source] = source;
+        visited[source] = true;
+
+        while (!q.empty()) {
+            int f = q.front();
+            cout << f << endl;
+            q.pop();
+
+            for (int nbr: this->l[f]) {
+                if (!visited[nbr]) {
+                    parent[nbr] = f;
+                    dist[nbr] = dist[f] + 1;
+                    q.push(nbr);
+                    visited[nbr] = true;
+                }
+            }
+        }
+
+        if (dest != -1) {
+            int temp = dest;
+            while (parent[temp] != temp) {
+                cout << temp << ", ";
+                temp = parent[temp];
+            }
+            cout << endl;
+        }
+    }
+};
+
+
+int main(){
+	Graph g(7);
+	g.addEdge(0,1);
+	g.addEdge(1,2);
+	g.addEdge(2,3);
+	g.addEdge(3,5);
+	g.addEdge(5,6);
+	g.addEdge(4,5);
+	g.addEdge(0,4);
+	g.addEdge(3,4);
+    g.showGraph();
+	g.bfs(1, 6);
+	return 0;
+}
