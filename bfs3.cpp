046b585f9656e7c25dcec32ca5af2c18abd23f9d
dsdjoy4@gmail.com <dsdjoy4@gmail.com>
2022-01-04 02:11:56 +1100

    bfs lol

---
+++ bfs3.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <list>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+class Graph {
+public:
+    int V;
+    list<int> *l;
+
+    Graph(int v) {
+        this->V = v;
+        this->l = new list<int>[this->V];
+    }
+
+    void addEdge(int i, int j) {
+        this->l[i].push_back(j);
+        this->l[j].push_back(i);
+    }
+
+    void bfs(int source) {
+        vector<bool> visited(this->V, false);
+        queue<int> q;
+        q.push(source);
+        visited[source] = true;
+
+        while(!q.empty()) {
+            int f = q.front();
+            cout << f << ", ";
+            q.pop();
+
+            for (int nbr: this->l[f]) {
+                if (!visited[nbr]) {
+                    q.push(nbr);
+                    visited[nbr] = true;
+                }
+            }
+        }
+        cout << endl;
+    }
+
+    void showGraph() {
+        for (int i = 0; i < this->V; i++) {
+            cout << i << ": ";
+            for (auto vertex: this->l[i]) {
+                cout << vertex << ", ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main(void) {
+    Graph g(7);
+    g.addEdge(1,2);
+    g.addEdge(1,0);
+    g.addEdge(2,3);
+    g.addEdge(3,5);
+    g.addEdge(3,4);
+    g.addEdge(0,4);
+    g.addEdge(4,6);
+    g.addEdge(5,6);
+    g.showGraph();
+    g.bfs(1);
+
+    return 0;
+}
