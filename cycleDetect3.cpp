657c0a0c5ec70d96489df4cac22798f2279f6974
DebashishSarkarDurjoy <dsdjoy4@gmail.com>
2022-03-20 01:02:35 +1100

    graph practice: detecting cycle

---
+++ cycleDetect3.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <list>
+#include <vector>
+
+using namespace std;
+
+class Graph {
+    int V;
+    list<int> *l;
+
+public:
+    Graph(int V) {
+        this->V = V;
+        this->l = new list<int> [this->V];
+    }
+
+    void addEdge(int i, int j) {
+        this->l[i].push_back(j);
+        this->l[j].push_back(i);
+    }
+
+    void showGraph() {
+        for (int i = 0; i < this->V; i++) {
+            cout << i << ": ";
+            for (auto node: this->l[i]) {
+                cout << node << ", ";
+            }
+            cout << endl;
+        }
+    }
+
+    bool isCyclic(int start) {
+        vector<bool> visited(this->V, false);
+        return dfs(start, visited, -1);
+    }
+
+    bool dfs(int node, vector<bool> &visited, int parent) {
+        visited[node] = true;
+        for (auto nbr: this->l[node]) {
+            if (!visited[nbr]) {
+                bool isCyclePresent = dfs(nbr, visited, node);
+                if (isCyclePresent) return true;
+            }
+            else if (nbr != parent) return true;
+        }
+        return false;
+    }
+};
+
+int main(void) {
+    Graph g(6);
+      g.addEdge(0,1);
+      g.addEdge(0,4);
+      g.addEdge(2,1);
+      g.addEdge(3,4);
+      g.addEdge(4,5);
+      g.addEdge(2,3);
+      g.addEdge(3,5);
+    g.showGraph();
+
+    if (g.isCyclic(0)) cout << "Cycle Present" << endl;
+    else cout << "Cycle not present" << endl;
+
+    return 0;
+}
